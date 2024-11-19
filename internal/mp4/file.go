a86feb8ba394dc382eea39480a629ed2721ac157
Alex Gustafsson <alex-gustafsson@live.se>
2024-11-19 17:05:41 +0100

    Implement function to allocate gap in file

--- internal/mp4/file.go
+++ internal/mp4/file.go
@@ -49,6 +49,62 @@ func (f *File) SeekAtom(needle string) (int64, uint32, error) {
 	}
 }
 
+// Allocate allocates a chunk of size bytes at offset.
+// That is, it truncates the underlying file and "moves" (copies) the data at
+// offset size bytes further in.
+func (f *File) Allocate(offset int64, size int64) error {
+	fileOffset, err := f.Seek(0, io.SeekCurrent)
+	if err != nil {
+		return err
+	}
+
+	// Keep the current offset even if we allocate a gap before it
+	if fileOffset > offset {
+		fileOffset += size
+	}
+
+	stat, err := f.Stat()
+	if err != nil {
+		return err
+	}
+
+	if err := f.Truncate(stat.Size() + size); err != nil {
+		return err
+	}
+
+	_, err = f.Seek(offset+size, io.SeekStart)
+	if err != nil {
+		return err
+	}
+
+	readFile, err := os.Open(f.Name())
+	if err != nil {
+		return err
+	}
+
+	_, err = readFile.Seek(offset, io.SeekStart)
+	if err != nil {
+		readFile.Close()
+		return err
+	}
+
+	reader := io.LimitReader(readFile, stat.Size()-offset)
+
+	_, err = io.Copy(f, reader)
+	if err != nil {
+		readFile.Close()
+		return err
+	}
+
+	_, err = readFile.Seek(fileOffset, io.SeekStart)
+	if err != nil {
+		readFile.Close()
+		return err
+	}
+
+	return readFile.Close()
+}
+
 // ReadAtomHeader reads the header of an atom at the current location.
 // Returns its size and type.
 func (f *File) ReadAtomHeader() (uint32, string, error) {
